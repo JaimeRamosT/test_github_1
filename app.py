c65cee545b8c6128ccf8796546b1ea9ec89e9316
JaimeRamosT <jaime.ramos@utec.edu.pe>
2023-08-17 21:02:32 -0500

    Multiplicacion

--- app.py
+++ app.py
@@ -9,6 +9,19 @@ app = Flask(__name__)
  
 
 @app.route('/')
-
 def hello():
     return '<h1>Hello, There!</h1>'
+
+
+
+@app.route('/multiplicar')
+def multiplicar():
+    # Obtener los parámetros desde la URL
+    num1 = int(request.args.get("num1"))
+    num2 = int(request.args.get("num2"))
+
+    # Calcular la multiplicacion
+    result = num1 * num2
+
+    # Retornar el resultado como una cadena de texto
+    return f"La multiplicacion de {num1} y {num2} es: {result}"
